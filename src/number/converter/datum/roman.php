b272c6702e844ff445c01f635d5c73a4eab3fdbb
Frédéric Hardy <frederic.hardy@mageekbox.net>
2017-06-21 14:21:16 +0200

    Add support for all current romanian representation for any natural number. Add data provider in romanian converter test class.
    Conclusion: the romanian representation must be provided by a depedency!
    
    WARNING! This commit introduce a SRP violation, because the romanian converter class has two responsibilities:
    
    1. breaking-up the decimal number according to constraint of roman number (no zero) ;
    2. finding the romanian representation according to break-up with a mapping.
    
    So, this class does not respect the SRP, because:
    
    1. If a new romanian representation is added (for example `DD` for 666), the class must be updated accordingly ;
    2. If the algorithm to break-up the decimal number is updated (for example, `0` must have a romanian representation), the class must be updated accordingly.

--- src/number/converter/datum/roman.php
+++ src/number/converter/datum/roman.php
@@ -19,25 +19,50 @@ class roman
 
 	function numberValueIs(int $value) :void
 	{
-		$datumValue = null;
-
-		switch ($value)
+		if ($value > 0)
 		{
-			case 1:
-				$datumValue = 'I';
-				break;
+			$datumValue = '';
 
-			case 5:
-				$datumValue = 'V';
-				break;
+			// Currently, this class has two responsibilities:
+			// 1. break-up the decimal number ;
+			// 2. find the romanian representation according to break-up.
+			// So, this class does not respect the SRP, because:
+			// 1. If a new romanian representation is added (for example `DD` for 666), the class must be updated accordingly ;
+			// 2. If the algorithm to break-up the decimal number is updated (for example, `0` has now a romanian representation), the class must be updated accordingly.
+			// Conclusion: the romanian representation must be provided by a depedency!
+			$mapping = [
+				1000 => 'M',
+				900 => 'CM',
+				500 => 'D',
+				400 => 'CD',
+				100 => 'C',
+				90 => 'XC',
+				50 => 'L',
+				40 => 'XL',
+				10 => 'X',
+				9 => 'IX',
+				5 => 'V',
+				4 => 'IV',
+				1 => 'I',
+			];
 
-			case 10:
-				$datumValue = 'X';
-				break;
-		}
+			$roman = reset($mapping);
+			$limit = key($mapping);
 
-		if ($datumValue)
-		{
+			while ($value > 0)
+			{
+				if ($value < $limit)
+				{
+					$roman = next($mapping);
+					$limit = key($mapping);
+				}
+				else
+				{
+					$datumValue .= $roman;
+					$value -= $limit;
+				}
+			}
+
 			$this->template
 				->recipientOfDatumWithValueIs(
 					$datumValue,
--- tests/units/src/number/converter/datum/roman.php
+++ tests/units/src/number/converter/datum/roman.php
@@ -14,15 +14,14 @@ class roman extends units\test
 		;
 	}
 
-	function testNumberValueIs()
+	function testNumberValueIs_withZero()
 	{
 		$this
 			->given(
-				$template = new mockOfDatum,
-				$recipient = new mockOfDatum\recipient
+				$this->newTestedInstance($template = new mockOfDatum, $recipient = new mockOfDatum\recipient)
 			)
 			->if(
-				$this->newTestedInstance($template, $recipient)->numberValueIs(0)
+				$this->testedInstance->numberValueIs(0)
 			)
 			->then
 				->object($this->testedInstance)
@@ -30,66 +29,57 @@ class roman extends units\test
 				->mock($recipient)
 					->receive('datumIs')
 						->never
+		;
+	}
 
+	/**
+	 * @dataProvider validNumberValueProvider
+	 */
+	function testNumberValueIs($numberValue, $romanValue)
+	{
+		$this
 			->given(
-				$i = new mockOfDatum,
-				$this->calling($template)->recipientOfDatumWithValueIs = function($value, $recipient) use ($i) {
-					if ($value == 'I')
+				$this->newTestedInstance($template = new mockOfDatum, $recipient = new mockOfDatum\recipient),
+
+				$roman = new mockOfDatum,
+				$this->calling($template)->recipientOfDatumWithValueIs = function($value, $recipient) use ($romanValue, $roman) {
+					if ($value == $romanValue)
 					{
-						$recipient->datumIs($i);
+						$recipient->datumIs($roman);
 					}
 				}
 			)
 			->if(
-				$this->newTestedInstance($template, $recipient)->numberValueIs(1)
+				$this->testedInstance->numberValueIs($numberValue)
 			)
 			->then
 				->object($this->testedInstance)
 					->isEqualTo($this->newTestedInstance($template, $recipient))
 				->mock($recipient)
 					->receive('datumIs')
-						->withArguments($i)
-							->once
-
-			->given(
-				$v = new mockOfDatum,
-				$this->calling($template)->recipientOfDatumWithValueIs = function($value, $recipient) use ($v) {
-					if ($value == 'V')
-					{
-						$recipient->datumIs($v);
-					}
-				}
-			)
-			->if(
-				$this->newTestedInstance($template, $recipient)->numberValueIs(5)
-			)
-			->then
-				->object($this->testedInstance)
-					->isEqualTo($this->newTestedInstance($template, $recipient))
-				->mock($recipient)
-					->receive('datumIs')
-						->withArguments($v)
-							->once
-
-			->given(
-				$x = new mockOfDatum,
-				$this->calling($template)->recipientOfDatumWithValueIs = function($value, $recipient) use ($x) {
-					if ($value == 'X')
-					{
-						$recipient->datumIs($x);
-					}
-				}
-			)
-			->if(
-				$this->newTestedInstance($template, $recipient)->numberValueIs(10)
-			)
-			->then
-				->object($this->testedInstance)
-					->isEqualTo($this->newTestedInstance($template, $recipient))
-				->mock($recipient)
-					->receive('datumIs')
-						->withArguments($x)
+						->withArguments($roman)
 							->once
 		;
+	}
+
+	protected function validNumberValueProvider()
+	{
+		return [
+			'1' => [ 1, 'I' ],
+			'5' => [ 5, 'V' ],
+			'10' => [ 10, 'X' ],
+			'50' => [ 50, 'L' ],
+			'100' => [ 100, 'C' ],
+			'500' => [ 500, 'D' ],
+			'1000' => [ 1000, 'M' ],
+			'2' => [ 2, 'II' ],
+			'4' => [ 4, 'IV' ],
+			'9' => [ 9, 'IX' ],
+			'40' => [ 40, 'XL' ],
+			'90' => [ 90, 'XC' ],
+			'400' => [ 400, 'CD' ],
+			'900' => [ 900, 'CM' ],
+			'4888' => [ 4888, 'MMMMDCCCLXXXVIII' ]
+		];
 	}
 }
